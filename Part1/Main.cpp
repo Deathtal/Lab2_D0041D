3276720e34bfee66ed6914d676e29d0c55a5df52
Herman Åberg <herrman.aberrg@gmail.com>
2020-03-11 17:37:35 +0100

    .

--- Part1/Main.cpp
+++ Part1/Main.cpp
@@ -2,35 +2,45 @@
 #include <cstdlib>
 #include "Sorting.h"
 
-static const size_t ARRAY_SIZE = 12;
+static const size_t ARRAY_SIZE = 1000;
+static const int MAX_RAND_VALUE = 100;
+static const int K = 69;
 
 template<typename T, size_t N>
 void fillArray(T arr[N], const int& max_value) {
-	for (int i = 0; i < N; ++i) {
+	for (size_t i = 0; i < N; ++i) {
 		arr[i] = rand() % (max_value + 1);
 	}
 }
 
 int main() {
 	int a[ARRAY_SIZE];
-	fillArray<int, ARRAY_SIZE>(a, 100);
-	/*for (auto i : a) {
+	srand(10);
+	fillArray<int, ARRAY_SIZE>(a, MAX_RAND_VALUE);
+	sort<int, bInsertionSort>(a, ARRAY_SIZE, K);
+	/*std::cout << "\n";
+	for (auto i : a) {
 		std::cout << i << ", ";
 	}*/
-	std::cout << "\n";
-	std::cout << "\n";
-	aSort<int>(a, ARRAY_SIZE, 4);
-	/*for (int i = 1; i < ARRAY_SIZE; ++i) {
-		if (a[i-1] > a[i]) {
+	for (int i = 1; i < ARRAY_SIZE; ++i) {
+		if (a[i - 1] > a[i]) {
 			std::cout << "\n\n";
 			std::cout << "ERROR at index " << i - 1 << " and " << i << ", value " << a[i - 1] << " more than " << a[i];
 			std::cout << "\n";
 		}
-	}*/
-	std::cout << "\n";
-	std::cout << "\n";
-	for (auto i : a) {
+	}
+	srand(10);
+	fillArray<int, ARRAY_SIZE>(a, MAX_RAND_VALUE);
+	sort<int, insertionSort>(a, ARRAY_SIZE, K);
+	/*for (auto i : a) {
 		std::cout << i << ", ";
 	}
-	std::cout << "\n";
+	std::cout << "\n";*/
+	for (int i = 1; i < ARRAY_SIZE; ++i) {
+		if (a[i - 1] > a[i]) {
+			std::cout << "\n\n";
+			std::cout << "ERROR at index " << i - 1 << " and " << i << ", value " << a[i - 1] << " more than " << a[i];
+			std::cout << "\n";
+		}
+	}
 }
--- Part1/Sorting.h
+++ Part1/Sorting.h
@@ -1,7 +1,7 @@
 #pragma once
 
 template<typename T>
-void insertionSort(T arr[], size_t N) {
+void insertionSort(T arr[], int N) {
 	for (size_t i = 1; i < N; ++i) {
 		for (size_t ii = 0; ii < i; ++ii) {
 			if (arr[i] >= arr[ii]) {
@@ -16,9 +16,8 @@ void insertionSort(T arr[], size_t N) {
 	}
 }
 
-
 template<typename T>
-int binarySearch(T arr[], T item, size_t begin_i, size_t end_i) {
+int binarySearch(T arr[], T item, int begin_i, int end_i) {
 	if (begin_i >= end_i) {
 		return (item > arr[begin_i]) ? (begin_i + 1) : begin_i;
 	}
@@ -28,12 +27,12 @@ int binarySearch(T arr[], T item, size_t begin_i, size_t end_i) {
 	if (arr[(begin_i + end_i) / 2] < item) {
 		return binarySearch<T>(arr, item, (begin_i + end_i) / 2 + 1, end_i);
 	}
-	return binarySearch<T>(arr, item, begin_i, (begin_i + end_i) / 2 - 1);
+	return binarySearch<T>(arr, item, begin_i, (begin_i + end_i + 1) / 2 - 1);
 }
 
-template<typename T, size_t N>
-void bInsertionSort(T arr[N]) {
-	int shift_i, selected, selected_pos;
+template<typename T>
+void bInsertionSort(T arr[], int N) {
+	int selected, shift_i, selected_pos;
 	for (int i = 1; i < N; ++i) {
 		shift_i = i - 1;
 		selected = arr[i];
@@ -46,44 +45,61 @@ void bInsertionSort(T arr[N]) {
 	}
 }
 
-
 template<typename T>
-void merge(T arr[], size_t N, size_t k) {
-	T* left_arr = new T[N];
-	T* right_arr = new T[k];
-	for (int i = 0; i < k; ++i) {
-		left_arr[i] = arr[i];
-	}
-	for (int i = k; i < k * 2; ++i) {
+void merge(T arr[], int arrL_size, int arrR_size, T wsArr[]) {
+	int arrL_i = 0;
+	int arrR_i = arrL_size;
+	int wsArr_i = 0;
 
+	while (wsArr_i < arrL_size + arrR_size) {
+		if ((arrR_i < arrL_size + arrR_size && arr[arrL_i] > arr[arrR_i]) || arrL_i > arrL_size - 1) {
+			wsArr[wsArr_i] = arr[arrR_i];
+			arrR_i++;
+		}
+		else {
+			wsArr[wsArr_i] = arr[arrL_i];
+			arrL_i++;
+		}
+		wsArr_i++;
+	}
+	for (int i = 0; i < arrL_size + arrR_size; ++i) {
+		arr[i] = wsArr[i];
 	}
 }
 
-
-template<typename T>
-void aSort(T arr[], size_t N, size_t k) {
+template<typename T, void (*func)(T*, int)>
+void mergeSort(T arr[], int N, int k, T wsArr[]) {
 	if (N <= k) {
-		insertionSort<T>(arr, N);
+		func(arr, N);
 		
-		std::cout << N << ":[ "; // This is just so that I can show off how good my array looks when divided
-		for (size_t i = 0; i < N; ++i) {
-			std::cout << arr[i] << " ";
-		}
-		std::cout << "]  ";
+		//std::cout << N << ":[ "; // This is just so that I can show off how good my array looks when divided
+		//for (int i = 0; i < N; ++i) {
+		//	std::cout << arr[i] << " ";
+		//}
+		//std::cout << "]  ";
 		
 		return;
 	}
 	if (N % k == 0) { // For dividing the array into as many pieces of size k as possible. I wanted to figure out how to do it, so now it's here
-		aSort<T>(arr, N - k * ((N / k) / 2), k);
-		aSort<T>(arr + N - k * ((N / k) / 2), k * ((N / k) / 2), k);
+		mergeSort<T, func>(arr, N - k * ((N / k) / 2), k, wsArr);
+		mergeSort<T, func>(arr + N - k * ((N / k) / 2), k * ((N / k) / 2), k, wsArr);
+		merge<T>(arr, N - k * ((N / k) / 2), k * ((N / k) / 2), wsArr);
 	}
 	else if (N <= N % k + N) { // Also for print prettiness
-		aSort<T>(arr, k, k);
-		aSort<T>(arr + k, N - k, k);
+		mergeSort<T, func>(arr, k, k, wsArr);
+		mergeSort<T, func>(arr + k, N - k, k, wsArr);
+		merge<T>(arr, k, N - k, wsArr);
 	}
 	else { // Can use only this without the other if-statements if desired, but it wouldn't look as cool when you print out the divisions
-		aSort<T>(arr, N / 2, k);
-		aSort<T>(arr + N / 2, N / 2, k);
+		mergeSort<T, func>(arr, N / 2, k, wsArr);
+		mergeSort<T, func>(arr + N - N / 2, N - N / 2, k, wsArr);
+		merge<T>(arr, N / 2, N - N / 2, wsArr);
 	}
-	merge<T>(arr, N, k);
+}
+
+template<typename T, void(*func)(T*, int)>
+void sort(T arr[], int N, int k) {
+	T* work_space_arr = new T[N];
+	mergeSort<T, func>(arr, N, k, work_space_arr);
+	delete[] work_space_arr;
 }
