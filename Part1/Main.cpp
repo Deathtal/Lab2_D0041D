79ee64388642428fb70c878ff896db7f44ea1aa2
Deathtal <herrman.aberrg@gmail.com>
2020-03-11 12:19:19 +0100

    .

--- Part1/Main.cpp
+++ Part1/Main.cpp
@@ -2,28 +2,35 @@
 #include <cstdlib>
 #include "Sorting.h"
 
+static const size_t ARRAY_SIZE = 12;
+
 template<typename T, size_t N>
-void fillArray(T arr[N]) {
+void fillArray(T arr[N], const int& max_value) {
 	for (int i = 0; i < N; ++i) {
-		arr[i] = rand() % 1001;
+		arr[i] = rand() % (max_value + 1);
 	}
 }
 
 int main() {
-	int a[128];
-	fillArray<int, 128>(a);
+	int a[ARRAY_SIZE];
+	fillArray<int, ARRAY_SIZE>(a, 100);
+	/*for (auto i : a) {
+		std::cout << i << ", ";
+	}*/
+	std::cout << "\n";
+	std::cout << "\n";
+	aSort<int>(a, ARRAY_SIZE, 4);
+	/*for (int i = 1; i < ARRAY_SIZE; ++i) {
+		if (a[i-1] > a[i]) {
+			std::cout << "\n\n";
+			std::cout << "ERROR at index " << i - 1 << " and " << i << ", value " << a[i - 1] << " more than " << a[i];
+			std::cout << "\n";
+		}
+	}*/
+	std::cout << "\n";
+	std::cout << "\n";
 	for (auto i : a) {
 		std::cout << i << ", ";
 	}
 	std::cout << "\n";
-	std::cout << "\n";
-	bInsertionSort<int, 128>(a);
-	for (auto i : a) {
-		std::cout << i << ", ";
-	}
-	std::cout << "\n";
-	insertionSort<int, 128>(a);
-	for (auto i : a) {
-		std::cout << i << ", ";
-	}
 }
--- Part1/Sorting.h
+++ Part1/Sorting.h
@@ -1,7 +1,7 @@
 #pragma once
 
-template<typename T, size_t N>
-void insertionSort(T arr[N]) {
+template<typename T>
+void insertionSort(T arr[], size_t N) {
 	for (size_t i = 1; i < N; ++i) {
 		for (size_t ii = 0; ii < i; ++ii) {
 			if (arr[i] >= arr[ii]) {
@@ -16,8 +16,9 @@ void insertionSort(T arr[N]) {
 	}
 }
 
+
 template<typename T>
-int binarySearch(T arr[], T item, int begin_i, int end_i) {
+int binarySearch(T arr[], T item, size_t begin_i, size_t end_i) {
 	if (begin_i >= end_i) {
 		return (item > arr[begin_i]) ? (begin_i + 1) : begin_i;
 	}
@@ -43,4 +44,46 @@ void bInsertionSort(T arr[N]) {
 		}
 		arr[shift_i + 1] = selected;
 	}
+}
+
+
+template<typename T>
+void merge(T arr[], size_t N, size_t k) {
+	T* left_arr = new T[N];
+	T* right_arr = new T[k];
+	for (int i = 0; i < k; ++i) {
+		left_arr[i] = arr[i];
+	}
+	for (int i = k; i < k * 2; ++i) {
+
+	}
+}
+
+
+template<typename T>
+void aSort(T arr[], size_t N, size_t k) {
+	if (N <= k) {
+		insertionSort<T>(arr, N);
+		
+		std::cout << N << ":[ "; // This is just so that I can show off how good my array looks when divided
+		for (size_t i = 0; i < N; ++i) {
+			std::cout << arr[i] << " ";
+		}
+		std::cout << "]  ";
+		
+		return;
+	}
+	if (N % k == 0) { // For dividing the array into as many pieces of size k as possible. I wanted to figure out how to do it, so now it's here
+		aSort<T>(arr, N - k * ((N / k) / 2), k);
+		aSort<T>(arr + N - k * ((N / k) / 2), k * ((N / k) / 2), k);
+	}
+	else if (N <= N % k + N) { // Also for print prettiness
+		aSort<T>(arr, k, k);
+		aSort<T>(arr + k, N - k, k);
+	}
+	else { // Can use only this without the other if-statements if desired, but it wouldn't look as cool when you print out the divisions
+		aSort<T>(arr, N / 2, k);
+		aSort<T>(arr + N / 2, N / 2, k);
+	}
+	merge<T>(arr, N, k);
 }
